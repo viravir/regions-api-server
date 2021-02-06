861b6584bc3f3f539af13e2df0d0eb78d361f65a
Victor Krivonos <viranaaret@gmail.com>
2021-02-06 17:00:08 +0500

    use pool.query instead of client.query to prevent risk of leaking connections

--- src/repositories/RegionRepository.ts
+++ src/repositories/RegionRepository.ts
@@ -24,54 +24,40 @@ type DeleteParams = {
 
 class RegionRepository {
   public async getAll(): Promise<Region[]> {
-    const client = await pool.connect()
-
     const queryStatement = `SELECT * FROM ${cfg.dbTableName} ORDER BY path;`
-    const { rows } = await client.query(queryStatement)
-
-    client.release()
+    const { rows } = await pool.query(queryStatement)
 
     return rows
   }
 
   public async get(params: GetParams): Promise<Region> {
-    const client = await pool.connect()
-
     const queryStatement = params.id
       ? `SELECT * FROM ${cfg.dbTableName} WHERE id = $1;`
       : `SELECT * FROM ${cfg.dbTableName} WHERE path = $1;`
     const queryValues = [params.id || params.path]
-    const { rows } = await client.query(queryStatement, queryValues)
-
-    client.release()
+    const { rows } = await pool.query(queryStatement, queryValues)
 
     return rows[0]
   }
 
   public async add(params: AddParams): Promise<Region> {
-    const client = await pool.connect()
-
     const queryStatement = `INSERT INTO ${cfg.dbTableName} (name, path) VALUES ($1, $2) RETURNING *;`
     const queryValues = [params.name, params.path]
-    const { rows } = await client.query(queryStatement, queryValues)
-
-    client.release()
+    const { rows } = await pool.query(queryStatement, queryValues)
 
     return rows[0]
   }
 
   public async update(params: UpdateParams): Promise<void> {
-    const client = await pool.connect()
-
     if (params.name !== undefined) {
       const queryStatement = `UPDATE ${cfg.dbTableName} set name = $1 WHERE id = $2;`
       const queryValues = [params.name, params.id]
-      await client.query(queryStatement, queryValues)
+      await pool.query(queryStatement, queryValues)
     }
     if (params.path) {
       const getCurrentPathStatement = `SELECT path FROM ${cfg.dbTableName} WHERE id = $1;`
       const queryValues = [params.id]
-      const { rows } = await client.query(getCurrentPathStatement, queryValues)
+      const { rows } = await pool.query(getCurrentPathStatement, queryValues)
 
       const currentPath = rows[0].path
 
@@ -80,32 +66,26 @@ class RegionRepository {
         // move target node
         const targetNodeQueryStatement = `UPDATE ${cfg.dbTableName} set path = $1 WHERE path = $2;`
         const targetNodeQueryValues = [params.path, currentPath]
-        await client.query(targetNodeQueryStatement, targetNodeQueryValues)
+        await pool.query(targetNodeQueryStatement, targetNodeQueryValues)
 
         // attach children nodes to node with updated path
         const childrenNodesQueryStatement = `UPDATE ${cfg.dbTableName} set path = $1 || subpath(path, nlevel($2)) WHERE path <@ $2;`
         const childrenNodesQueryValues = [params.path, currentPath]
-        await client.query(childrenNodesQueryStatement, childrenNodesQueryValues)
+        await pool.query(childrenNodesQueryStatement, childrenNodesQueryValues)
       }
     }
-
-    client.release()
   }
 
   public async delete(params: DeleteParams): Promise<void> {
-    const client = await pool.connect()
-
     const getCurrentPathStatement = `SELECT path FROM ${cfg.dbTableName} WHERE id = $1;`
     const getCurrentPathQueryValues = [params.id]
-    const { rows: pathFindRows } = await client.query(getCurrentPathStatement, getCurrentPathQueryValues)
+    const { rows: pathFindRows } = await pool.query(getCurrentPathStatement, getCurrentPathQueryValues)
 
     const currentPath = pathFindRows[0].path
 
     const queryStatement = `DELETE FROM ${cfg.dbTableName} WHERE path <@ $1;`
     const queryValues = [currentPath]
-    await client.query(queryStatement, queryValues)
-
-    client.release()
+    await pool.query(queryStatement, queryValues)
   }
 }
 
