48889197f1232eec3a233b51f12460d7599630a9
Victor Krivonos <viranaaret@gmail.com>
2021-02-06 16:50:34 +0500

    remove useless try/catch blocks (they were added as a boilerplate pattern, but now they make code less readable)

--- src/repositories/RegionRepository.ts
+++ src/repositories/RegionRepository.ts
@@ -24,88 +24,72 @@ type DeleteParams = {
 
 class RegionRepository {
   public async getAll(): Promise<Region[]> {
-    try {
-      const client = await pool.connect()
+    const client = await pool.connect()
 
-      const queryStatement = `SELECT * FROM ${cfg.dbTableName} ORDER BY path;`
-      const { rows } = await client.query(queryStatement)
+    const queryStatement = `SELECT * FROM ${cfg.dbTableName} ORDER BY path;`
+    const { rows } = await client.query(queryStatement)
 
-      client.release()
+    client.release()
 
-      return rows
-    } catch (e) {
-      throw new Error(e)
-    }
+    return rows
   }
 
   public async get(params: GetParams): Promise<Region> {
-    try {
-      const client = await pool.connect()
+    const client = await pool.connect()
 
-      const queryStatement = params.id
-        ? `SELECT * FROM ${cfg.dbTableName} WHERE id = $1;`
-        : `SELECT * FROM ${cfg.dbTableName} WHERE path = $1;`
-      const queryValues = [params.id || params.path]
-      const { rows } = await client.query(queryStatement, queryValues)
+    const queryStatement = params.id
+      ? `SELECT * FROM ${cfg.dbTableName} WHERE id = $1;`
+      : `SELECT * FROM ${cfg.dbTableName} WHERE path = $1;`
+    const queryValues = [params.id || params.path]
+    const { rows } = await client.query(queryStatement, queryValues)
 
-      client.release()
+    client.release()
 
-      return rows[0]
-    } catch (e) {
-      throw new Error(e)
-    }
+    return rows[0]
   }
 
   public async add(params: AddParams): Promise<Region> {
-    try {
-      const client = await pool.connect()
+    const client = await pool.connect()
 
-      const queryStatement = `INSERT INTO ${cfg.dbTableName} (name, path) VALUES ($1, $2) RETURNING *;`
-      const queryValues = [params.name, params.path]
-      const { rows } = await client.query(queryStatement, queryValues)
+    const queryStatement = `INSERT INTO ${cfg.dbTableName} (name, path) VALUES ($1, $2) RETURNING *;`
+    const queryValues = [params.name, params.path]
+    const { rows } = await client.query(queryStatement, queryValues)
 
-      client.release()
+    client.release()
 
-      return rows[0]
-    } catch (e) {
-      throw new Error(e)
-    }
+    return rows[0]
   }
 
   public async update(params: UpdateParams): Promise<void> {
-    try {
-      const client = await pool.connect()
+    const client = await pool.connect()
 
-      if (params.name !== undefined) {
-        const queryStatement = `UPDATE ${cfg.dbTableName} set name = $1 WHERE id = $2;`
-        const queryValues = [params.name, params.id]
-        await client.query(queryStatement, queryValues)
-      }
-      if (params.path) {
-        const getCurrentPathStatement = `SELECT path FROM ${cfg.dbTableName} WHERE id = $1;`
-        const queryValues = [params.id]
-        const { rows } = await client.query(getCurrentPathStatement, queryValues)
-
-        const currentPath = rows[0].path
-
-        if (currentPath !== params.path) {
-          // TODO -> figure out how to do it via single operation
-          // move target node
-          const targetNodeQueryStatement = `UPDATE ${cfg.dbTableName} set path = $1 WHERE path = $2;`
-          const targetNodeQueryValues = [params.path, currentPath]
-          await client.query(targetNodeQueryStatement, targetNodeQueryValues)
-
-          // attach children nodes to node with updated path
-          const childrenNodesQueryStatement = `UPDATE ${cfg.dbTableName} set path = $1 || subpath(path, nlevel($2)) WHERE path <@ $2;`
-          const childrenNodesQueryValues = [params.path, currentPath]
-          await client.query(childrenNodesQueryStatement, childrenNodesQueryValues)
-        }
-      }
-
-      client.release()
-    } catch (e) {
-      throw new Error(e)
+    if (params.name !== undefined) {
+      const queryStatement = `UPDATE ${cfg.dbTableName} set name = $1 WHERE id = $2;`
+      const queryValues = [params.name, params.id]
+      await client.query(queryStatement, queryValues)
     }
+    if (params.path) {
+      const getCurrentPathStatement = `SELECT path FROM ${cfg.dbTableName} WHERE id = $1;`
+      const queryValues = [params.id]
+      const { rows } = await client.query(getCurrentPathStatement, queryValues)
+
+      const currentPath = rows[0].path
+
+      if (currentPath !== params.path) {
+        // TODO -> figure out how to do it via single operation
+        // move target node
+        const targetNodeQueryStatement = `UPDATE ${cfg.dbTableName} set path = $1 WHERE path = $2;`
+        const targetNodeQueryValues = [params.path, currentPath]
+        await client.query(targetNodeQueryStatement, targetNodeQueryValues)
+
+        // attach children nodes to node with updated path
+        const childrenNodesQueryStatement = `UPDATE ${cfg.dbTableName} set path = $1 || subpath(path, nlevel($2)) WHERE path <@ $2;`
+        const childrenNodesQueryValues = [params.path, currentPath]
+        await client.query(childrenNodesQueryStatement, childrenNodesQueryValues)
+      }
+    }
+
+    client.release()
   }
 
   public async delete(params: DeleteParams): Promise<void> {
--- src/services/RegionService.ts
+++ src/services/RegionService.ts
@@ -28,61 +28,41 @@ class RegionsService {
   }
 
   public getAll = async (): Promise<Region[]> => {
-    try {
-      const regions = await this.repository.getAll()
+    const regions = await this.repository.getAll()
 
-      return regions
-    } catch (e) {
-      throw new Error(e)
-    }
+    return regions
   }
 
   public get = async (params: GetParams): Promise<Region> => {
-    try {
-      const region = await this.repository.get({ id: params.id })
+    const region = await this.repository.get({ id: params.id })
 
-      return region
-    } catch (e) {
-      throw new Error(e)
-    }
+    return region
   }
 
   public add = async (params: AddParams): Promise<Region> => {
-    try {
-      const existingRegionAtPath = await this.repository.get({ path: params.path })
-      if (existingRegionAtPath) {
-        throw new Error('Region already exists at provided path')
-      }
-      const region = await this.repository.add({ name: params.name, path: params.path })
-
-      return region
-    } catch (e) {
-      throw new Error(e)
+    const existingRegionAtPath = await this.repository.get({ path: params.path })
+    if (existingRegionAtPath) {
+      throw new Error('Region already exists at provided path')
     }
+    const region = await this.repository.add({ name: params.name, path: params.path })
+
+    return region
   }
 
   public update = async (params: UpdateParams): Promise<void> => {
-    try {
-      const existingRegionAtPath = await this.repository.get({ path: params.path })
-      if (existingRegionAtPath) {
-        throw new Error('Region already exists at provided path')
-      }
-      await this.repository.update({ id: params.id, name: params.name, path: params.path })
-    } catch (e) {
-      throw new Error(e)
+    const existingRegionAtPath = await this.repository.get({ path: params.path })
+    if (existingRegionAtPath) {
+      throw new Error('Region already exists at provided path')
     }
+    await this.repository.update({ id: params.id, name: params.name, path: params.path })
   }
 
   public delete = async (params: DeleteParams): Promise<void> => {
-    try {
-      const existingRegionAtPath = await this.repository.get({ id: params.id })
-      if (!existingRegionAtPath) {
-        throw new Error('Region does not exist')
-      }
-      await this.repository.delete({ id: params.id })
-    } catch (e) {
-      throw new Error(e)
+    const existingRegionAtPath = await this.repository.get({ id: params.id })
+    if (!existingRegionAtPath) {
+      throw new Error('Region does not exist')
     }
+    await this.repository.delete({ id: params.id })
   }
 }
 
